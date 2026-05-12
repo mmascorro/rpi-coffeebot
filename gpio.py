8526c24667023257b438f95482b2b069cc048d40
Miguel Mascorro <miguel@lumenbrite.com>
2026-05-12 15:00:52 -0500

    updates to gpio pin handling

--- gpio.py
+++ gpio.py
@@ -1,13 +1,13 @@
-from gpiozero import DigitalOutputDevice
+from gpiozero import LED
 
 def turn_on(pin):
-	od = DigitalOutputDevice(pin)
-	od.off()
-
-def turn_off(pin):
-	od = DigitalOutputDevice(pin)
+	od = LED(pin, active_high=False)
 	od.on()
 
+def turn_off(pin):
+	od = LED(pin, active_high=False)
+	od.off()
+
 def pin_status(pin):
-	od = DigitalOutputDevice(pin)
+	od = LED(pin, active_high=False)
 	return od.value
